368f129d0052390ab2c8bcd35d8fcf35238c4bcb
Forofonof <DanielResult1@outlook.com>
2022-04-03 17:01:03 +0300

    Добавьте файлы проекта.

---
+++ Homework_07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string userMessage;
+        int numberOfMessages;
+
+        Console.WriteLine("Введите слово, которое выведет консоль: ");
+        userMessage = Console.ReadLine();
+        Console.WriteLine("Сколько раз консоль выведет слово: ");
+        numberOfMessages = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < numberOfMessages; i++)
+        {
+            Console.WriteLine(userMessage);
+        }
+    }
+}
